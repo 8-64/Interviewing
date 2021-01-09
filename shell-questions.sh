63789f8ffb204605a564ad1936d2957e7718a1c7
8-64 <basilwashere@gmail.com>
2021-01-09 07:40:25 +0100

    + 2 shell questions

--- shell-questions.sh
+++ shell-questions.sh
@@ -16,3 +16,13 @@ Second
 Third
 " > ./testgrep.txt
 grep -E -A 2 --no-group-separator '^Host.*[0-9]{2}' $(find . -name *.txt)
+
+# 2) Print the first and the last lines of the command output
+ps -ef | tee >(tail -1 > last.txt) | head -1; cat last.txt
+# OR, in Perl:
+ps -ef | perl -nE "say if 1..1 or eof"
+
+# 3) Print the first and the third coulums of the command output
+ls -ahl | cut -f1,3 -d' '
+# OR, in Perl:
+ls -ahl | perl -F'\s+' -E 'say "@F[0,2]"
